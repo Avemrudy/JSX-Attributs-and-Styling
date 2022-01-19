1d7448d2b2edc7b8b9c533a67647e15ad67b0840
Avemrudy <97418419+Avemrudy@users.noreply.github.com>
2022-01-19 02:48:05 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>codesandbox is cancer</h1>
+    <ul>
+      <li>
+        <img
+          src="https://image.freepik.com/free-vector/fuck-you-symbol-comic-style_52683-47788.jpg"
+          className="sandboxCancer"
+        />
+      </li>
+      <li>
+        <img
+          src="https://pbs.twimg.com/profile_images/1466434875553157131/lqrxLMbj_400x400.png"
+          className="fuckCodesandbox"
+        />
+      </li>
+      <li>
+        <img
+          src="https://i.pinimg.com/736x/8a/45/31/8a4531b19e5f01ca7635d15a53241bd2.jpg"
+          className="sandBoxMoreLikeTrashBox"
+        />
+      </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
